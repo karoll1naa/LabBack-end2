74125f63d49b5307efb31d2d283697c2c2edbd8b
karoll1naa <karollinnajan@gmail.com>
2024-10-19 17:03:34 +0300

    Adding project files

---
+++ src/main/java/main/labbackend2/LabBackEnd2Application.java
@@ -0,0 +1,13 @@
+package main.labbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabBackEnd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabBackEnd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/main/labbackend2/LabBackEnd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.labbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabBackEnd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
