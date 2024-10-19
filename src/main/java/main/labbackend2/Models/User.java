ae0ca60d3d4a923bdd8ef2c469d5424801740f32
karoll1naa <karollinnajan@gmail.com>
2024-10-19 17:11:34 +0300

    Data structure for the user

---
+++ src/main/java/main/labbackend2/Models/User.java
@@ -0,0 +1,19 @@
+package main.labbackend2.Models;
+
+public class User {
+    private long id;
+    private String username;
+
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+}
